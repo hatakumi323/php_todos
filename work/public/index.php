6a22748eaf0fd9bed06c7366121e9ee9edcebd3a
hatakumi323 <takuken135@gmail.com>
2021-09-20 16:41:22 +0900

    php create

---
+++ work/public/index.php
@@ -0,0 +1,82 @@
+<?php
+
+define('DSN', 'mysql:host=db;dbname=myapp;charset=utf8mb4');
+define('DB_USER', 'myappuser');
+define('DB_PASS', 'myapppass');
+
+try {
+  $pdo = new PDO(
+    DSN,
+    DB_USER,
+    DB_PASS,
+    [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+      PDO::ATTR_EMULATE_PREPARES => false
+    ]
+  );
+} catch (PDOException $e) {
+  echo $e->getMessage();
+  exit;
+}
+
+function h($str)
+{
+  return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+}
+
+function addTodo($pdo)
+{
+  $title = trim(filter_input(INPUT_POST, 'title'));
+  if ($title === '') {
+    return;
+  }
+
+  $stmt = $pdo->prepare("INSERT INTO todos (title) VALUES (:title)");
+  $stmt->bindValue('title', $title, PDO::PARAM_STR);
+  $stmt->execute();
+}
+
+function getTodos($pdo)
+{
+  $stmt = $pdo->query("SELECT * FROM todos ORDER BY id DESC");
+  $todos = $stmt->fetchAll();
+  return $todos;
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  addTodo($pdo);
+}
+
+$todos = getTodos($pdo);
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8">
+  <title>My Todos</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+  <h1>Todos</h1>
+
+  <form action="" method="POST">
+    <input type="text" name="title" placeholder="Type new todo.">
+  </form>
+
+  <ul>
+    <?php foreach ($todos as $todo) : ?>
+      <li>
+        <input type="checkbox" name="" id="" <?= $todo->is_done ? 'checked' : ''; ?>>
+        <span class="<?= $todo->is_done ? 'done' : ''; ?>">
+          <?= h($todo->title); ?>
+        </span>
+      </li>
+    <?php endforeach; ?>
+  </ul>
+</body>
+
+</html>
